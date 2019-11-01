084f25950d5648d0e1ffe61b47dab6fa5dbb3751
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2019-11-01 12:13:20 +0100

    Basic movement (walk and jump) working

---
+++ Nano Doctor/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerMovement player;
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        GetMovementInput();
+    }
+
+    void GetMovementInput()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+            player.Move(true);
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+            player.Move(false);
+    }
+
+    void GetInput()
+    {
+        if (Input.GetButtonDown("Jump") && player.canJump)
+            player.Jump();
+    }
+}
--- Nano Doctor/Assets/Scripts/PlayerMovement.cs
+++ Nano Doctor/Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,39 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float jumpForce;
-    private float moveInput;
 
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb2D;
+
+    public bool canJump;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    void Awake()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        canJump = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+
     void Update()
     {
-        
+        rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+    }
+
+    public void Move(bool faceRight)
+    {
+        if (faceRight)
+            rb2D.velocity = new Vector2(speed * Time.fixedDeltaTime, rb2D.velocity.y);
+        else
+            rb2D.velocity = new Vector2(-speed * Time.fixedDeltaTime, rb2D.velocity.y);
+    }
+
+    public void Jump()
+    {
+        rb2D.velocity = Vector2.up * jumpForce;
     }
 }
