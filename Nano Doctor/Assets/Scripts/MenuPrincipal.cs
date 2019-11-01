d26e2da2869ee34ec17454330809bb68b5162180
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2019-11-01 18:38:22 +0100

    Main menu added (buttons working)

---
+++ Nano Doctor/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Debug.Log("Has salido del juego correctamente.");
+        Application.Quit();
+    }
+
+    public void goToTestScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
